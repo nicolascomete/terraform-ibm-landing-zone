7416afb3fadf1cf7912cde87b2d835eb6acc89f9
nicolascomete <nicolas.comete@gmail.com>
2022-10-18 19:50:41 +0200

    Update main.tf

--- kms/main.tf
+++ kms/main.tf
@@ -47,7 +47,14 @@ data "ibm_resource_instance" "hpcs_instance" {
 # Create Key Rings
 ##############################################################################
 
-resource "ibm_kms_key_rings" "rings" {
+#resource "ibm_kms_key_rings" "rings" {
+#  for_each    = toset(local.key_rings)
+#  instance_id = local.key_management_guid
+#  key_ring_id = each.key
+#}
+    
+module "ibm_kms_key_rings" "rings" {
+  source      = "./dmzr-kms/key_ring"
   for_each    = toset(local.key_rings)
   instance_id = local.key_management_guid
   key_ring_id = each.key
@@ -60,18 +67,18 @@ resource "ibm_kms_key_rings" "rings" {
 # Create Keys
 ##############################################################################
 
-resource "ibm_kms_key" "key" {
-  for_each        = local.keys
-  instance_id     = local.key_management_guid
-  key_name        = each.value.name
-  standard_key    = each.value.root_key == null ? null : !each.value.root_key
-  payload         = each.value.payload
-  key_ring_id     = each.value.key_ring == null ? null : ibm_kms_key_rings.rings[each.value.key_ring].key_ring_id
-  force_delete    = each.value.force_delete != false ? true : each.value.force_delete
-  endpoint_type   = each.value.endpoint
-  iv_value        = each.value.iv_value
-  encrypted_nonce = each.value.encrypted_nonce
-}
+#resource "ibm_kms_key" "key" {
+#  for_each        = local.keys
+#  instance_id     = local.key_management_guid
+#  key_name        = each.value.name
+#  standard_key    = each.value.root_key == null ? null : !each.value.root_key
+#  payload         = each.value.payload
+#  key_ring_id     = each.value.key_ring == null ? null : ibm_kms_key_rings.rings[each.value.key_ring].key_ring_id
+#  force_delete    = each.value.force_delete != false ? true : each.value.force_delete
+#  endpoint_type   = each.value.endpoint
+#  iv_value        = each.value.iv_value
+#  encrypted_nonce = each.value.encrypted_nonce
+#}
 
 ##############################################################################
 
@@ -80,24 +87,24 @@ resource "ibm_kms_key" "key" {
 # Create Key Policies
 ##############################################################################
 
-resource "ibm_kms_key_policies" "key_policy" {
-  for_each      = local.policies
-  instance_id   = local.key_management_guid
-  endpoint_type = each.value.endpoint
-  key_id        = ibm_kms_key.key[each.key].key_id
-  # Dynamically create rotation block
-  dynamic "rotation" {
-    for_each = (each.value.policies.rotation == null ? [] : [each.value])
-    content {
-      interval_month = each.value.policies.rotation.interval_month
-    }
-  }
-  dynamic "dual_auth_delete" {
-    for_each = (each.value.policies.dual_auth_delete == null ? [] : [each.value])
-    content {
-      enabled = each.value.policies.dual_auth_delete.enabled
-    }
-  }
-}
+#resource "ibm_kms_key_policies" "key_policy" {
+#  for_each      = local.policies
+#  instance_id   = local.key_management_guid
+#  endpoint_type = each.value.endpoint
+#  key_id        = ibm_kms_key.key[each.key].key_id
+#  # Dynamically create rotation block
+#  dynamic "rotation" {
+#    for_each = (each.value.policies.rotation == null ? [] : [each.value])
+#    content {
+#      interval_month = each.value.policies.rotation.interval_month
+#    }
+#  }
+#  dynamic "dual_auth_delete" {
+#    for_each = (each.value.policies.dual_auth_delete == null ? [] : [each.value])
+#    content {
+#      enabled = each.value.policies.dual_auth_delete.enabled
+#    }
+#  }
+#}
 
 ##############################################################################
