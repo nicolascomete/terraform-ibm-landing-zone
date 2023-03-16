fc3123085c7265cfca9386c95b487374fae85828
nicolascomete <nicolas.comete@gmail.com>
2023-03-16 16:40:03 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,10 @@
 # Account Variables
 ##############################################################################
 
+variable "another_slz_exists_in_account" {
+  default = false
+}
+
 variable "prefix" {
   description = "A unique identifier for resources. Must begin with a letter and end with a letter or number. This prefix will be prepended to any resources provisioned by this template. Prefixes must be 16 or fewer characters."
   type        = string
