bb20a50a86502fbf6d824c88e263c4a808ff1ff4
nicolascomete <nicolas.comete@gmail.com>
2023-03-16 14:06:06 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ module "vpc" {
   network_acls                = each.value.network_acls
   use_public_gateways         = each.value.use_public_gateways
   subnets                     = each.value.subnets
-  routes                      = each.value.routes
+  routes                      = lookup(each.value, "routes", null)
 }
 
 
