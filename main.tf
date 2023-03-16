6af5090283d68db359ec1e6b93d1e00dac13da38
nicolascomete <nicolas.comete@gmail.com>
2023-03-16 14:01:09 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ module "vpc" {
   network_acls                = each.value.network_acls
   use_public_gateways         = each.value.use_public_gateways
   subnets                     = each.value.subnets
-  route                       = each.value.route
+  routes                      = each.value.routes
 }
 
 
